338eb8979479e27190b2b2e60a2d0b623ae3ea09
Aleem Isiaka <Aleem.Isiaka@gmail.com>
2020-06-06 16:49:28 +0100

    Completes API

--- tests/__tests__/carOwners.js
+++ tests/__tests__/carOwners.js
@@ -1,15 +1,27 @@
 const utils = require("../utils");
 const expect = require("expect");
+
 describe("/car-owners", () => {
   const server = utils.server();
+
+  const filter = {
+    countries: ["Brazil", "Ireland", "Egypt", "Peru"],
+    colors: ["Green", "Violet", "Yellow", "Blue"],
+    start_year: 2001,
+    end_year: 2009,
+    gender: "Male",
+  };
 
   afterAll(function (done) {
     server.close();
     setTimeout(() => done(), 500);
   });
 
-  const exec = (path = "/", method = "get") => {
-    return server[method]("/api" + path);
+  const exec = (path = "/", method = "get", body = {}) => {
+    return server[method]("/api" + path)
+      .send(JSON.stringify(body))
+      .set("Content-Type", "application/json")
+      .set("Accept", "application/json");
   };
 
   it("GET / exists", async () => {
@@ -25,8 +37,14 @@ describe("/car-owners", () => {
     expect(result.body.data.length).toBeGreaterThan(0);
   });
 
-  it("POST / exists", async () => {
+  it("POST /filter exists", async () => {
     const result = await exec("/car-owners/filter", "post");
+    expect(result.status).toBe(200);
+  });
+
+  it("POST /filter filters", async () => {
+    const result = await exec("/car-owners/filter", "post", filter);
+    console.log(result.body);
     expect(result.status).toBe(200);
   });
 });
