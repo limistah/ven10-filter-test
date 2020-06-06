2cc87aa1193422d94986860bcc076d7bb414627c
Aleem Isiaka <Aleem.Isiaka@gmail.com>
2020-06-06 16:59:51 +0100

    Page Layout

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./style.css";
+
+function _app(props) {
+  return <props.Component></props.Component>;
+}
+
+export default _app;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 import React from "react";
-
 function index() {
-  return <div>Hello!!!</div>;
+  return (
+    <div className="page">
+      <h2 className="header">Ven10 Filter MERN Test</h2>
+    </div>
+  );
 }
 
 export default index;
