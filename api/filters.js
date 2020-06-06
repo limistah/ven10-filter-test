b5daab6177c8f4c871d543af8e91341336b56595
Aleem Isiaka <Aleem.Isiaka@gmail.com>
2020-06-06 14:18:59 +0100

    Fetch data from db

--- api/filters.js
+++ api/filters.js
@@ -1,9 +1,21 @@
-function fetchFilters(req, res, next) {
+const Filters = require("../models/filters");
+
+async function fetchFilters(req, res, next) {
+  let { limit, page } = req.query;
+
+  limit = parseInt(limit || 10);
+  page = parseInt(page || 0);
+
+  const skip = limit * page;
+
+  const filters = await Filters.find({}).limit(limit).skip(skip);
+  const total = await Filters.find({}).countDocuments();
+
   return res.status(200).json({
-    data: [],
-    total: 0,
-    limit: 10,
-    page: 1,
+    data: filters,
+    total: total,
+    limit: limit,
+    page: page + 1,
   });
 }
 
