bc50613fc6049a7d417f9ac6e46ddfe6d18e220d
Aleem Isiaka <Aleem.Isiaka@gmail.com>
2020-06-06 13:51:08 +0100

    404 implementation

--- api/index.js
+++ api/index.js
@@ -4,5 +4,9 @@ module.exports = function apiRoutes(app) {
   router.get("/", function handler(req, res, next) {
     return res.status(200).json({ online: true });
   });
+  // Unregistered routes
+  router.all("*", function handler(req, res, next) {
+    return res.status(404).json({ error: { msg: "Not Found" } });
+  });
   return router;
 };
