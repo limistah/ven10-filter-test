c07f8660fa75826083218fb702a7e6b79f69b40f
Aleem Isiaka <Aleem.Isiaka@gmail.com>
2020-06-06 17:00:44 +0100

    Completes basic page structure

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,10 @@ function index() {
   return (
     <div className="page">
       <h2 className="header">Ven10 Filter MERN Test</h2>
+      <div className="content">
+        <div className="filters"></div>
+        <div className="results"></div>
+      </div>
     </div>
   );
 }
