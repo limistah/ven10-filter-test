994c78a22daba1d1a634591084f5029d2c5e0608
Aleem Isiaka <Aleem.Isiaka@gmail.com>
2020-06-06 14:53:34 +0100

    GET / car-owners returns data

--- tests/__tests__/carOwners.js
+++ tests/__tests__/carOwners.js
@@ -16,4 +16,12 @@ describe("/car-owners", () => {
     const result = await exec("/car-owners");
     expect(result.status).toBe(200);
   });
+
+  it("GET / returns data", async () => {
+    const result = await exec("/car-owners");
+    expect(result.status).toBe(200);
+    expect(result.body.data).toBeDefined();
+    expect(result.body.data.length).toBeDefined();
+    expect(result.body.data.length).toBeGreaterThan(0);
+  });
 });
