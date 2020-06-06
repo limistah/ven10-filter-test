7863bfb8e8c67212046f77ef92ef8f096bad5a15
Aleem Isiaka <Aleem.Isiaka@gmail.com>
2020-06-06 13:46:29 +0100

    Tests for index routes of the api

--- tests/__tests__/index.js
+++ tests/__tests__/index.js
@@ -9,18 +9,19 @@ describe("/", () => {
   });
 
   const exec = (path = "/") => {
-    return server.get("/api/" + path);
+    return server.get("/api" + path);
   };
 
   it("should be running", async () => {
     const result = await exec();
     expect(result.status).toBe(200);
-    expect(result.body.status).toBeTruthy();
-    expect(result.body.status).toBe("online");
+    expect(result.body.online).toBeTruthy();
+    expect(result.body.online).toBe(true);
   });
 
   it("should return 404 for unregistered routes", async () => {
     const result = await exec("/unknown");
+    // console.log(result);
     expect(result.status).toBe(404);
     expect(result.body.error).toBeTruthy();
     expect(result.body.error.msg).toBeTruthy();
